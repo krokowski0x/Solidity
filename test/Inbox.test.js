42bf0704c230da604772c5efd6ba8917ee1d6872
krokowski0x <krokowski.dx@gmail.com>
2018-08-08 14:21:03 +0200

    Added test suite for Inbox contract

---
+++ test/Inbox.test.js
@@ -0,0 +1,44 @@
+const assert =  require('assert');
+const ganache = require('ganache-cli');
+const Web3 =    require('web3');
+const { interface, bytecode } = require('../compile');
+
+// Create web3 instance and ganache test provider
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+// Global variables
+let accounts, inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy a contract
+  inbox = await new web3.eth
+    .Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hello!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  // Inject provider into web3 instance
+  inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hello!');
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('Hi!')
+      .send({ from: accounts[0] });
+
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi!');
+  });
+});
