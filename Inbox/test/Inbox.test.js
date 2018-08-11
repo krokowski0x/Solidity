2f356499e0007bcdeee5fd36644997a12ee58d15
krokowski0x <krokowski.dx@gmail.com>
2018-08-11 12:13:07 +0200

    Refactered tests to use chai's expect instead of assert

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -1,7 +1,7 @@
-const assert =  require('assert');
-const ganache = require('ganache-cli');
-const Web3 =    require('web3');
-const { interface, bytecode } = require('../compile');
+const expect = require("chai").expect;
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const { interface, bytecode } = require("../compile");
 
 // Create web3 instance and ganache test provider
 const provider = ganache.provider();
@@ -15,30 +15,28 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy a contract
-  inbox = await new web3.eth
-    .Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hello!'] })
-    .send({ from: accounts[0], gas: '1000000' });
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hello!"] })
+    .send({ from: accounts[0], gas: "1000000" });
 
   // Inject provider into web3 instance
   inbox.setProvider(provider);
 });
 
-describe('Inbox', () => {
-  it('deploys a contract', () => {
-    assert.ok(inbox.options.address);
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    expect(inbox.options.address).to.be.ok;
   });
 
-  it('has a default message', async () => {
+  it("has a default message", async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hello!');
+    expect(message).to.be.equal("Hello!");
   });
 
-  it('can change the message', async () => {
-    await inbox.methods.setMessage('Hi!')
-      .send({ from: accounts[0] });
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("Hi!").send({ from: accounts[0] });
 
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hi!');
+    expect(message).to.be.equal("Hi!");
   });
 });
