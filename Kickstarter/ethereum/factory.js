2a98bc147ad6357680024e93b144d50d2ac6e498
krokowski0x <krokowski.dx@gmail.com>
2018-08-14 13:18:23 +0200

    Got Next.js to work with Web3.js and render root route

--- Kickstarter/ethereum/factory.js
+++ Kickstarter/ethereum/factory.js
@@ -2,7 +2,7 @@ import web3 from "./web3";
 import CampaignFactory from "./build/CampaignFactory.json";
 const { ADDRESS } = require("./config");
 
-const instance = web3.eth.contract(
+const instance = new web3.eth.Contract(
 	JSON.parse(CampaignFactory.interface),
 	ADDRESS
 );
--- Kickstarter/pages/index.js
+++ Kickstarter/pages/index.js
@@ -1,1 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
+import factory from "../ethereum/factory";
+
+export default class CampaignIndex extends Component {
+	async componentDidMount() {
+		const campaigns = await factory.methods.getDeployedCampaigns().call();
+	}
+
+	render() {
+		return <div>Campaigns Index!</div>;
+	}
+}
