a0ac9c16b2558d66f9984087bcbb0f05f8cb88d3
krokowski0x <krokowski.dx@gmail.com>
2018-08-14 15:24:31 +0200

    Added basic semantic-ui components

--- Kickstarter/pages/index.js
+++ Kickstarter/pages/index.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 
 export default class CampaignIndex extends Component {
-	async componentDidMount() {
+	static async getInitialProps() {
 		const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+		return { campaigns };
+	}
+
+	renderCampaigns() {
+		const items = this.props.campaigns.map(address => {
+			return {
+				header: address,
+				description: <a>View Campaign</a>,
+				fluid: true
+			};
+		});
+		return <Card.Group items={items} />;
 	}
 
 	render() {
-		return <div>Campaigns Index!</div>;
+		return (
+			<div>
+				<link
+					rel="stylesheet"
+					href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
+				/>
+				{this.renderCampaigns()}
+				<Button content="Create Camapign" icon="add circle" primary />
+			</div>
+		);
 	}
 }
